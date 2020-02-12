ba9e590fc27e66ed24f61dc6d62353462d43d6a4
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-12 21:52:21 +0300

    refactor

--- app/src/main/java/com/bankmtk/listview/MainActivity.java
+++ app/src/main/java/com/bankmtk/listview/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout list = findViewById(R.id.layout);
+        LayoutInflater ltInflater =  getLayoutInflater();
+        final Activity that = this;
+        for (String city: cities){
+            View item = ltInflater.inflate(R.layout.item,list,false);
+            TextView cityView = item.findViewById(R.id.textView);
+            cityView.setText(city);
+            cityView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(that,String.format("Selected city - %s",
+                            ((TextView)v).getText()),Toast.LENGTH_SHORT).show();
+                }
+            });
+            list.addView(item);
+        }
+        /* //create element with code
         LinearLayout list = findViewById(R.id.layout);
         final Activity that = this;
         for (String city: cities){
@@ -30,6 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             list.addView(cityView);
-        }
+        }*/
     }
 }
